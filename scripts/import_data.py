efd4681f9f74d963c6b9b11565ae2efad7ffcaeb
Jack Boyd <jackkellyboyd@gmail.com>
2024-09-30 19:46:03 -0500

    Connect to postgres db via psycopg2 and minor updates to readme

--- scripts/import_data.py
+++ scripts/import_data.py
@@ -1,10 +1,23 @@
 import pandas as pd
+import os
 import psycopg2
 from sqlalchemy import create_engine
 
+#retrieve environment variables
+USER = os.getenv('DATABASE_USER')
+PASSWORD = os.getenv('DATABASE_PASSWORD')
+ENDPOINT = os.getenv("DATABASE_ENDPOINT")
+DATABASE = os.getenv('DATABASE_NAME')
+PORT = os.getenv('DATABASE_PORT')
+
 #load the excel file
-
 excelFile = 'data/liftingexceldoc.xlsx'
-df = pd.read_excel(excelFile, sheet_name= 'For DB - Lifts')
 
-print(df.head())
+#create the connection engine
+engine = create_engine(f'postgresql+psycopg2://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}')
+
+df = pd.read_excel(excelFile, sheet_name= 'For DB - Lifts')
+print(df)
+
+
+
