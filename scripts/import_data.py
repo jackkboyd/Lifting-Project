dbf0dac87147f7859a34d6c21b7984e6f31cf3a4
Jack Boyd <jackkellyboyd@gmail.com>
2024-10-11 21:35:09 -0500

    Update new members logic to iterate through multiple ID columns and add insert to fact table logic

--- scripts/import_data.py
+++ scripts/import_data.py
@@ -17,16 +17,42 @@ df = pd.read_excel(excelFile, sheet_name= 'For DB - Lifts')
 #create the connection engine
 engine = create_engine(f'postgresql+psycopg2://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}')
 
-#function to create new members and retrieve ID 
-def createRoutineNewMembers(connection, routineCode):
-    checkQuery = 'Select ID from lift."DimRoutines" where "RoutineCode" = %s'
-    result = connection.execute(checkQuery, (routineCode,)).fetchone()[0]
+def createNewMembers(connection, tableName, codeColumn, IDColumn, codeValue,):
+    """
+    function to create new members and retrieve ID 
+    Parameters:
+        -connection - the db connection
+        -tableName - dimension table we are inserting to / referencing
+        -codeColumn - column in the dimension table we are inserting / referncing
+        -IDColumn - associated Id of the dimension member
+        -codeValue - the actual code to insert / refer to 
+
+    Returns:
+        -Associated ID of the dimension member
+    """
+
+    checkQuery = f'Select "{IDColumn}" from lift."{tableName}" where "{codeColumn}" = %s'
+    result = connection.execute(checkQuery, (codeValue,)).fetchone()[0]
 
     if result:
-            return result['id']
+            return result['{IDColumn}']
     else:
-        insertQuery = 'INSERT INTO lift."DimRoutines" ("RoutineCode") VALUES (%s) RETURNING "RoutineID"'
-        newID = connection.execute(insertQuery, (routineCode,)).fetchone()[0]
+        insertDimQuery = f'INSERT INTO lift."{tableName}" ("{codeColumn}") VALUES (%s) RETURNING "{IDColumn}"'
+        newID = connection.execute(insertDimQuery, ({codeValue},)).fetchone()[0]
     return newID
+
+#create new members and insert into fact table
+with engine.connect() as connection:
+    for index, row in df.iterrows():
+        routineID = createNewMembers(connection, 'DimRoutines', 'RoutineCode', 'RoutineID', row['RoutineCode'])
+        workoutID = createNewMembers(connection, 'DimWorkouts', 'WorkoutCode', 'WorkoutID', row['WorkoutCode'])
+
+        #insert into lifts table
+        insertFactQuery = '''
+        INSERT INTO lifts."FactLifts" ("RoutineID","WorkoutID","MovementID","Reps1","Weight1","Reps2","Weight2","Reps3","Weight3","Reps4","Weight4","IsSuperset","IsSkipped","ApplyDate","Sequence","IsSub") 
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        '''
+        connection.execute(insertFactQuery,(routineID,workoutID,row['MovementName'],row['Reps1'],row['Weight1'],row['Reps2'],row['Weight2'],row['Reps3'],row['Weight3'],row['Reps4'],row['Weight4'],row['IsSuperset'],row['IsSkipped'],row['ApplyDate'],row['Sequence'],row['IsSub']))
+
 
 
