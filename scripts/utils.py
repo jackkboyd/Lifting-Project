caf20a5dacb052e2eb265dc87a0811b22211f3c6
Jack Boyd <jackkellyboyd@gmail.com>
2024-11-04 21:56:37 -0600

    update connection method to leverage aws secrets manager

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,6 +3,9 @@ import logging
 import psycopg
 from sqlalchemy import create_engine, text
 import pandas as pd
+import boto3
+from botocore.exceptions import ClientError
+import json
 
 #1 set up logging
 def setupLogger (log_file_name = 'app'):
@@ -17,22 +20,46 @@ def setupLogger (log_file_name = 'app'):
     )
     return logging.getLogger()
 
-#2 set up db connection
-def getDBCreds ():
-    USER = os.getenv('DATABASE_USER')
-    PASSWORD = os.getenv('DATABASE_PASSWORD')
-    ENDPOINT = os.getenv('DATABASE_ENDPOINT')
-    DATABASE = os.getenv('DATABASE_NAME')
-    PORT = os.getenv('DATABASE_PORT')
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
 
-    if not all([USER, PASSWORD, ENDPOINT, DATABASE, PORT]):
-        raise ValueError("Missing one or more database environment variables.")
+def get_secret():
 
-    return USER, PASSWORD, ENDPOINT, DATABASE, PORT
+    secret_name = "lift-credentials"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+    
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
 
 #3 create engine connection
 def setupConnection ():
-    USER, PASSWORD, ENDPOINT, DATABASE, PORT = getDBCreds()
+    creds = get_secret()
+
+    USER = creds["USER"]
+    PASSWORD = creds["PASSWORD"]
+    ENDPOINT = creds["ENDPOINT"]
+    DATABASE = creds["DATABASE"]
+    PORT = creds["PORT"]
+
     engine = create_engine(f'postgresql+psycopg://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}')
     return engine
 
